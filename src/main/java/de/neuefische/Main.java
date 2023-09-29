4d04c9eccf0c49e81ba2e448b6726d2275a1750e
Alex <alexander.unmuth@gmail.com>
2023-09-29 11:06:40 +0200

    Big Bang!

---
+++ src/main/java/de/neuefische/Main.java
@@ -0,0 +1,17 @@
+package de.neuefische;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+int = Integer
+double = Double
+float = Float
+char = Charakter
+boolean = Boolean
+*/
+
+public class Main {
+
+}
